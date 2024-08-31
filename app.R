dbc8fd80854d089595926065478423bb787abeea
apple <allen.chang@atgenomix.com>
2024-08-31 12:40:26 +0800

    feat: heatmap

--- app.R
+++ app.R
@@ -8,43 +8,20 @@
 #
 
 library(shiny)
+source("modules/rnaseq.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  tabsetPanel(
+    tabPanel("RNAseq",rnaseqUI("RNAseq")),
+  )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of binsd
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  rnaseqServer("RNAseq")
 }
 
 # Run the application 
---
+++ modules/rnaseq.R
@@ -0,0 +1,79 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+install.packages("sparklyr")
+install.packages("pysparklyr")
+pysparklyr::install_pyspark()
+
+library(shiny)
+library(sparklyr)
+library(dplyr)
+library(glue)
+library(DBI)
+library(pheatmap)
+
+# Connect to SeqsLab
+sc <- spark_connect(
+  master = "sc://localhost",
+  method = "spark_connect"
+)
+
+# list available databases (runs)
+dbs <- src_databases(sc)
+
+# Define UI for RNA-seq application
+rnaseqUI <- function(id) {
+  ns <- NS(id)
+  
+  selectInput(ns("db"), label = "Run name:", choices = dbs)
+  
+  tagList(
+    plotOutput(ns("heatmap"))
+  )
+}
+
+# Define server logic required to draw RNA-seq charts
+rnaseqServer <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    
+    read_table <- function(pattern, db, tbls) {
+      n <- tbls[grepl(pattern, tbls)]
+      spark_read_table(sc, glue("{db}.{n}"))
+    }
+    
+    tbls <- reactive({
+      tbl_change_db(sc, input$db)
+      dbGetQuery(sc, glue("SHOW TABLES IN {input$db}"))
+    })
+    
+    tbl_go <- reactive({
+      read_table("^go_delta", input$db, tbls())
+    })
+    
+    tbl_gosea <- reactive({
+      read_table("^gosea_delta", input$db, tbls())
+    })
+    
+    tbl_gopvalue <- reactive({
+      read_table("^gopvalue_delta", input$db, tbls())
+    })
+    
+    tbl_exacttest <- reactive({
+      read_table("^exacttest_delta", input$db, tbls())
+    })
+    
+    tbl_normalcounts <- reactive({
+      read_table("^normalcounts_delta", input$db, tbls())
+    })
+    
+    output$heatmap <- renderPlot({
+      pheatmap(tbl_normalcounts())
+    }, res = 96)
+  })
+}
